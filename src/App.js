da228a1f61fae917c4b29e1e6140dc3bfe9c80cf
amananand019 <aman019anand@gmail.com>
2023-07-05 00:53:22 +0530

    Added: Header, Loader and Error Components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Header from "./Header";
+
 export default function App() {
-  return <div>React Quiz</div>;
+  return (
+    <div>
+      <div className="app">
+        <Header />
+      </div>
+    </div>
+  );
 }
